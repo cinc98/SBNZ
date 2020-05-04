13883b0a6ec0effe1c89020ea41a7659e2cd7616
Ivan Cincurak <ivan.cincurak98@gmail.com>
2020-05-04 21:13:35 +0200

    init

---
+++ sbnz-project/sbnz-project/src/test/java/com/example/sbnzproject/SbnzProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbnzproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbnzProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
