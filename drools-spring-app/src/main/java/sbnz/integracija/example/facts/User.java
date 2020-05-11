1a4cc935550a0a8eb854e39fe80808af6a72aa68
Ivan Cincurak <ivan.cincurak98@gmail.com>
2020-05-12 00:08:44 +0200

    created connection with database

---
+++ drools-spring-app/src/main/java/sbnz/integracija/example/facts/User.java
@@ -0,0 +1,63 @@
+package sbnz.integracija.example.facts;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+
+	private String username;
+
+	private String password;
+
+	private Date registrationDate;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Date getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(Date registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+	public String getMedal() {
+		return medal;
+	}
+
+	public void setMedal(String medal) {
+		this.medal = medal;
+	}
+
+	private String medal;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
